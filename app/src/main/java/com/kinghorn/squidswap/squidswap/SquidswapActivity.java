4cc50d65092309dc451276c86d9e3af8819739e0
ddcjkinghorn <john.kinghorn@coxautoinc.com>
2018-03-09 17:27:59 -0500

    update

---
+++ app/src/main/java/com/kinghorn/squidswap/squidswap/SquidswapActivity.java
@@ -0,0 +1,13 @@
+package com.kinghorn.squidswap.squidswap;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SquidswapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squidswap);
+    }
+}
