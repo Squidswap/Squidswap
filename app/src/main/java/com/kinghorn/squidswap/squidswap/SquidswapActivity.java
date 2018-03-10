c45f0c82b31bdceac812f58ad9e5b7448e72bad9
Maxx730 <max.kinghorn@gmail.com>
2018-03-09 19:51:58 -0500

    update

--- app/src/main/java/com/kinghorn/squidswap/squidswap/SquidswapActivity.java
+++ app/src/main/java/com/kinghorn/squidswap/squidswap/SquidswapActivity.java
@@ -1,13 +1,71 @@
 package com.kinghorn.squidswap.squidswap;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class SquidswapActivity extends AppCompatActivity {
+
+    private ImageButton ImageButton,CameraButton;
+    private RelativeLayout CropCard,PaintCard,SwapCard;
+    private ImageView SelectedImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squidswap);
+    }
+
+    //Grab and set click events for the bottom tab buttons
+    private void InitializeBottomButtons(){
+        ImageButton = (ImageButton) findViewById(R.id.OpenImage);
+        CameraButton = (ImageButton) findViewById(R.id.CameraButton);
+
+        ImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        CameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    //Grab and set click events for the choice cards.
+    private void InitializeCards(){
+        CropCard = (RelativeLayout) findViewById(R.id.CropCard);
+        PaintCard = (RelativeLayout) findViewById(R.id.PaintCard);
+        SwapCard = (RelativeLayout) findViewById(R.id.SwapCard);
+
+        CropCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        PaintCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        SwapCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
