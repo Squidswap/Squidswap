ae7d4357f7be5a5ffa4dc670df6503b694b5c315
Maxx730 <max.kinghorn@gmail.com>
2018-03-09 21:27:58 -0500

    update

--- app/src/main/java/com/kinghorn/squidswap/squidswap/SquidswapActivity.java
+++ app/src/main/java/com/kinghorn/squidswap/squidswap/SquidswapActivity.java
@@ -1,5 +1,7 @@
 package com.kinghorn.squidswap.squidswap;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,17 +10,36 @@ import android.view.WindowInsets;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class SquidswapActivity extends AppCompatActivity {
 
     private ImageButton ImageButton,CameraButton;
     private RelativeLayout CropCard,PaintCard,SwapCard;
     private ImageView SelectedImage;
+    private Bitmap ChosenImage;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK){
+            switch(requestCode){
+                case 1:
+
+                    break;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_squidswap);
+
+        SelectedImage = (ImageView) findViewById(R.id.ImagePreview);
+
+        InitializeBottomButtons();
     }
 
     //Grab and set click events for the bottom tab buttons
@@ -29,7 +50,13 @@ public class SquidswapActivity extends AppCompatActivity {
         ImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), 1);
             }
         });
 
@@ -64,7 +91,7 @@ public class SquidswapActivity extends AppCompatActivity {
         SwapCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                
+
             }
         });
     }
